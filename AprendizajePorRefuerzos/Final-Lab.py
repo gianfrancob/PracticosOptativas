73cf40cd119e75138a1ba00a7ebb5791dbf8801d
Gianfranco Barbiani <gianfranco.barbiani@gmail.com>
2019-10-26 12:29:58 -0300

    Added 2nd part theory. Changed gamma 2  with gamma 0.1, and re-run notebook. Changed some conclusions. Added Final-Lab.py (for testting porpuses only)

--- AprendizajePorRefuerzos/Final-Lab.py
+++ AprendizajePorRefuerzos/Final-Lab.py
@@ -1,38 +1,47 @@
-# Inspired by https://keon.io/deep-q-learning/
+import matplotlib
+matplotlib.rcParams['figure.figsize'] = 16, 8
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-dark')
 
-import gym
 import math
 import numpy as np
+import warnings
+warnings.simplefilter('ignore')
+import gym
+import time
+from IPython.display import clear_output
+import itertools
+
+import random
+from collections import deque
 import torch
 import torch.nn as nn
-
+import torch.nn.functional as F
 from agents.utils.memory.ReplayMemory import ReplayMemory
 from agents.utils.memory.Transition import Transition
-from sklearn.preprocessing import PolynomialFeatures
 
+## CartPole: Agente aleatorio
+'''
+CartPole es un entorno donde un poste está unido por una unión no accionada a un carro,
+que se mueve a lo largo de una pista sin fricción. El sistema se controla aplicando una 
+fuerza de +1 o -1 al carro. El péndulo comienza en posición vertical, y el objetivo es 
+evitar que se caiga. Se proporciona una recompensa de +1 por cada paso de tiempo que el
+poste permanezca en posición vertical. El episodio termina cuando el poste está a más
+de 15 grados de la vertical, o el carro se mueve más de 2.4 unidades desde el centro.
+'''
+env = gym.make('CartPole-v0')
+env.reset()
+for _ in range(250):
+    env.render(mode='human')
+    observation, reward, done, info = env.step(env.action_space.sample()) # se ejecuta una acción aleatoria
+    if done:
+        env.reset()
+env.close()
+clear_output()
 
-class LinearModel(nn.Module):
-
-    def __init__(self, _input_size: int, _output_size: int):
-        super(LinearModel, self).__init__()
-        self.output = nn.Linear(_input_size, _output_size)
-
-        # init weights
-        for m in self.modules():
-            if isinstance(m, nn.Linear):
-                nn.init.xavier_uniform_(m.weight)
-                m.bias.data.fill_(0.01)
-
-    # Called with either one element to determine next action, or a batch
-    # during optimization. Returns tensor([[left0exp,right0exp]...]).
-    def forward(self, x):
-        x = self.output(x)
-        return x
-
-
-class PolyCartPoleSolver:
-    def __init__(self, env, n_episodes=1000, max_env_steps=None, gamma=0.5,
-                 epsilon=1.0, epsilon_min=0.01, epsilon_log_decay=0.005, alpha=1e-3,
+class LinearCartPoleSolver:
+    def __init__(self, env, n_episodes=3000, max_env_steps=None, gamma=0.9,
+                 epsilon=0.5, epsilon_min=0.01, epsilon_log_decay=0.005, alpha=1e-3,
                  memory_size=10000, batch_size=256, render=False, debug=False):
 
         self.memory = ReplayMemory(capacity=memory_size)
@@ -51,10 +60,6 @@ class PolyCartPoleSolver:
         self.observation_space_size = env.observation_space.shape[0]
         self.action_space_size = env.action_space.n
 
-        self.feature_tuning = PolynomialFeatures(interaction_only=True)
-        # Initialize feature tunning
-        self.feature_tuning.fit(self.env.reset().reshape((1, 4)))
-
         self.render = render
         self.debug = debug
         if debug:
@@ -69,7 +74,7 @@ class PolyCartPoleSolver:
         # linear function, and holds internal Tensors for its weight and bias.
         # After constructing the model we use the .to() method to move it to the
         # desired device.
-        self.model = LinearModel(self.feature_tuning.n_output_features_, self.action_space_size).to(self.device)
+        self.model = LinearModel(self.observation_space_size, self.action_space_size).to(self.device)
         self.model.train()
 
         # The nn package also contains definitions of popular loss functions; in this
@@ -85,15 +90,6 @@ class PolyCartPoleSolver:
         # optimization algoriths. The first argument to the Adam constructor tells the
         # optimizer which Tensors it should update.
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=alpha)
-
-    def preprocess_state(self, state):
-        """State and action are stacked horizontally and its features are combined as a polynomial to be passed as an
-        input of the approximator"""
-
-        # poly_state converts the horizontal stack into a combination of its parameters i.e.
-        # [1, s_1, s_2, s_3, s_4, a_1, s_1 s_2, s_1 s_3, ...]
-        poly_state = self.feature_tuning.transform(state.reshape((1, 4)))
-        return poly_state[0]
 
     def choose_action(self, state, epsilon):
         """Chooses the next action according to the model trained and the policy"""
@@ -165,7 +161,7 @@ class PolyCartPoleSolver:
         mean_scores = []
         for e in range(self.n_episodes):
             state = self.env.reset()
-            state = torch.tensor(self.preprocess_state(state), device=self.device, dtype=torch.float)
+            state = torch.tensor(state, device=self.device, dtype=torch.float)
             done = False
             cum_reward = 0
             while not done:
@@ -173,11 +169,11 @@ class PolyCartPoleSolver:
                     state,
                     self.get_epsilon(e))
                 next_state, reward, done, _ = self.env.step(action)
-                next_state = torch.tensor(self.preprocess_state(next_state), device=self.device, dtype=torch.float)
+                next_state = torch.tensor(next_state, device=self.device, dtype=torch.float)
 
                 cum_reward += reward
                 self.memory.push(
-                    state,  # Converted to tensor before
+                    state,  #Converted to tensor before
                     torch.tensor([action], device=self.device),
                     None if done else next_state,
                     torch.tensor(reward, device=self.device).clamp_(-1, 1),
@@ -202,8 +198,3 @@ class PolyCartPoleSolver:
 
     def load(self, path):
         self.model = torch.load(path)
-
-
-if __name__ == '__main__':
-    agent = PolyCartPoleSolver(gym.make('CartPole-v0'), debug=True)
-    agent.run()
